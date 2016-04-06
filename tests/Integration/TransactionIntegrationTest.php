27e02f25e0cd5943c99c1587ac7f9ad74a63a699
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-04-06 17:41:03 +0200

    bolt wrapped tx test

--- tests/Integration/TransactionIntegrationTest.php
+++ tests/Integration/TransactionIntegrationTest.php
@@ -55,7 +55,59 @@ class TransactionIntegrationTest extends IntegrationTestCase
     public function testPushShouldStackUntilCommit()
     {
         $this->emptyDb();
-        $tx = $this->client->transaction();
+        $tx = $this->client->transaction('http');
+        $tx->push('CREATE (n:Test)');
+        $this->assertNotContains($tx->status(), [Transaction::COMMITED, Transaction::ROLLED_BACK, Transaction::OPENED]);
+        $this->assertXNodesWithLabelExist('Test', 0);
+        $tx->push('CREATE (n:Test)');
+        $this->assertXNodesWithLabelExist('Test', 0);
+        $tx->commit();
+        $this->assertXNodesWithLabelExist('Test', 2);
+        $this->assertEquals(Transaction::COMMITED, $tx->status());
+    }
+
+    public function testTransactionIsCommittedWithBolt()
+    {
+        $this->emptyDb();
+        $tx = $this->client->transaction('bolt');
+        $result = $tx->run('CREATE (n:Test) RETURN id(n) as id');
+        $this->assertTrue($result->summarize()->updateStatistics()->containsUpdates());
+        $this->assertEquals(Transaction::OPENED, $tx->status());
+        $tx->commit();
+        $this->assertEquals(Transaction::COMMITED, $tx->status());
+        $this->assertXNodesWithLabelExist('Test');
+    }
+
+    public function testTransactionIsRolledBackWithBolt()
+    {
+        $this->emptyDb();
+        $tx = $this->client->transaction('bolt');
+        $result = $tx->run('CREATE (n:Test) RETURN id(n) as id');
+        $this->assertTrue($result->summarize()->updateStatistics()->containsUpdates());
+        $this->assertEquals(Transaction::OPENED, $tx->status());
+        $tx->rollback();
+        $this->assertEquals(Transaction::ROLLED_BACK, $tx->status());
+        $this->assertXNodesWithLabelExist('Test', 0);
+    }
+
+    public function testTransactionIsRolledBackInCaseOfExceptionWithBolt()
+    {
+        $this->emptyDb();
+        $tx = $this->client->transaction('bolt');
+        try {
+            $result = $tx->run('CREATE (n:Test) RETURN x');
+            $this->assertEquals(1, 2); // If we reached here then there is a bug
+        } catch (Neo4jException $e) {
+            $this->assertEquals(Neo4jExceptionInterface::EFFECT_ROLLBACK, $e->effect());
+        }
+        $this->assertEquals(Transaction::ROLLED_BACK, $tx->status());
+        $this->assertXNodesWithLabelExist('Test', 0);
+    }
+
+    public function testPushShouldStackUntilCommitWithBolt()
+    {
+        $this->emptyDb();
+        $tx = $this->client->transaction('bolt');
         $tx->push('CREATE (n:Test)');
         $this->assertNotContains($tx->status(), [Transaction::COMMITED, Transaction::ROLLED_BACK, Transaction::OPENED]);
         $this->assertXNodesWithLabelExist('Test', 0);
