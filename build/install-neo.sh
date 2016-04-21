5fda8da9e46ef1b045721a265c42e3f430ec44c3
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-04-21 14:32:48 +0200

    fix travis 2

--- build/install-neo.sh
+++ build/install-neo.sh
@@ -6,7 +6,5 @@ export JRE_HOME=/usr/lib/jvm/java-8-oracle
 wget http://dist.neo4j.org/neo4j-enterprise-3.0.0-RC1-unix.tar.gz > null
 mkdir neo
 tar xzf neo4j-enterprise-3.0.0-RC1-unix.tar.gz -C neo --strip-components=1 > null
-sed -i.bak '/\(dbms\.security\.auth_enabled=\).*/s/^#//g' conf/neo4j.conf
+sed -i.bak '/\(dbms\.security\.auth_enabled=\).*/s/^#//g' ./neo/conf/neo4j.conf
 neo/bin/neo4j start > null &
-#sleep 10
-#mv neo/conf/ssl/snakeoil.cert neo/conf/ssl/snakeoil.pem
