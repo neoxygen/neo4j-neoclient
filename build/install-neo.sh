35866112b5212dc9189b9fe15311b30f41496fc8
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-04-21 14:24:26 +0200

    new sed command for disabling auth

--- build/install-neo.sh
+++ build/install-neo.sh
@@ -6,8 +6,7 @@ export JRE_HOME=/usr/lib/jvm/java-8-oracle
 wget http://dist.neo4j.org/neo4j-enterprise-3.0.0-RC1-unix.tar.gz > null
 mkdir neo
 tar xzf neo4j-enterprise-3.0.0-RC1-unix.tar.gz -C neo --strip-components=1 > null
-sed -i.bak 's/^\(dbms\.security\.auth_enabled=\).*/\1false/' ./neo/conf/neo4j.conf
-#sed -i.bak 's/^\(dbms\.bolt\.tls\.enabled=\).*/\1true/' ./neo/conf/neo4j-server.properties
+sed -i.bak '/\(dbms\.security\.auth_enabled=\).*/s/^#//g' conf/neo4j.conf
 neo/bin/neo4j start > null &
 #sleep 10
 #mv neo/conf/ssl/snakeoil.cert neo/conf/ssl/snakeoil.pem
