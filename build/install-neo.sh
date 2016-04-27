74dbc308b6a78b880be9f627b8cf9bad44ce1ecd
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-04-27 17:00:44 +0100

    revert multienv for now

--- build/install-neo.sh
+++ build/install-neo.sh
@@ -3,8 +3,8 @@
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export JRE_HOME=/usr/lib/jvm/java-8-oracle
 
-wget http://dist.neo4j.org/neo4j-enterprise-$NEO_VERSION-unix.tar.gz > null
+wget http://dist.neo4j.org/neo4j-enterprise-3.0.0-unix.tar.gz > null
 mkdir neo
-tar xzf neo4j-enterprise-$NEO_VERSION-unix.tar.gz -C neo --strip-components=1 > null
+tar xzf neo4j-enterprise-3.0.0-unix.tar.gz -C neo --strip-components=1 > null
 sed -i.bak '/\(dbms\.security\.auth_enabled=\).*/s/^#//g' ./neo/conf/neo4j.conf
 neo/bin/neo4j start > null &
