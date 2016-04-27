62af6dc603dc17eed5a3ce95006e1004030c476f
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-04-27 12:36:06 +0100

    upgrade tests to 3.0 final

--- build/install-neo.sh
+++ build/install-neo.sh
@@ -3,7 +3,7 @@
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export JRE_HOME=/usr/lib/jvm/java-8-oracle
 
-wget http://dist.neo4j.org/neo4j-enterprise-3.0.0-RC1-unix.tar.gz > null
+wget http://dist.neo4j.org/neo4j-enterprise-3.0.0-unix.tar.gz > null
 mkdir neo
 tar xzf neo4j-enterprise-3.0.0-RC1-unix.tar.gz -C neo --strip-components=1 > null
 sed -i.bak '/\(dbms\.security\.auth_enabled=\).*/s/^#//g' ./neo/conf/neo4j.conf
